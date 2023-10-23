09727c7fe5e8b96ed4ffb1f5c168662b417299f4
josepvazquezp <jose.vazquez@iteso.mx>
2023-10-22 19:32:39 -0600

    proyectil direction

--- projectile.py
+++ projectile.py
@@ -6,7 +6,11 @@ from settings import *
 class Projectile(pygame.sprite.Sprite):
     def __init__(self,x,y,angle):
         super().__init__()
-        self.image = pygame.image.load('Assets/Projectiles/Icon-KnifeBG.jpg').convert_alpha()
+        self.image = pygame.image.load('Assets/Projectiles/navaja.png').convert_alpha()
+        if(angle != 0):
+            self.image = pygame.transform.rotate(self.image, angle)
+            if(angle == 270 or angle == 90):
+                self.image = pygame.transform.flip(self.image,False,True)
         self.image = pygame.transform.rotozoom(self.image,0,.25)
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
@@ -18,7 +22,7 @@ class Projectile(pygame.sprite.Sprite):
         self.y_vel = self.speed * math.sin(math.radians(self.angle))
         self.projectile_lifetime = BULLET_LIFETIME
         self.spawn_time = pygame.time.get_ticks()
-    
+
     def projectile_movement(self):
         self.x += self.x_vel
         self.y += self.y_vel
